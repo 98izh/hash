5e440f278bb32a99f5d92349ff4567a1213924fb
Ismail Hamad <ismail.hamad@alawnehpay.com>
2025-02-19 20:02:20 +0300

    removing profile

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,10 +3,7 @@ java --version
 cd /home/proxidize/app/
 echo "Starting app hash"
 FNAME=$(ls -1 /home/proxidize/app/hash.jar)
-ENVIRONMENT=assignment
-
 
 java $JAVAOPS \
      -javaagent:/home/proxidize/jmx_prometheus_javaagent.jar=9090:/home/proxidize/config.yaml \
-     -Dspring.profiles.active=$ENVIRONMENT \
      -jar $FNAME
