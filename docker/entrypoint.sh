d53d22a13d0a34d99c4b13ef2489be8caf2307e0
Ismail Hamad <ismail.hamad@alawnehpay.com>
2025-02-19 19:36:23 +0300

    editing Dockerfile and entrypoint for OT auto instrumentation

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,6 +3,10 @@ java --version
 cd /home/proxidize/app/
 echo "Starting app hash"
 FNAME=$(ls -1 /home/proxidize/app/hash.jar)
+ENVIRONMENT=assignment
 
-java -javaagent:/home/proxidize/jmx_prometheus_javaagent.jar=9090:/home/proxidize/config.yaml -jar \
--Dspring.profiles.active=$ENVIRONMENT $JAVAOPS $FNAME
+
+java $JAVAOPS \
+     -javaagent:/home/proxidize/jmx_prometheus_javaagent.jar=9090:/home/proxidize/config.yaml \
+     -Dspring.profiles.active=$ENVIRONMENT \
+     -jar $FNAME
